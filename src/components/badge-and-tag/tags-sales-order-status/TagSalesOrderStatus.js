0f3927af2f2a0e870ed69f274258de2a2b1800fb
SORRANUN BUMRUNGPONG <gaiougame000000@gmail.com>
2024-12-23 16:26:52 +0700

    SO status

--- src/components/badge-and-tag/tags-sales-order-status/TagSalesOrderStatus.js
+++ src/components/badge-and-tag/tags-sales-order-status/TagSalesOrderStatus.js
@@ -20,6 +20,28 @@ function TagSalesOrderStatus({result}) {
     case 'ยกเลิก':
       elementToRender = <Tag icon={<CloseCircleFilledIcon />} color="#ababab"> ยกเลิก </Tag>;
       break;
+    case 'ชำระยังไม่ครบ':
+      elementToRender = <Tag icon={<ClockCircleFilled />} color="#454545"> ชำระยังไม่ครบ </Tag>;
+      break;
+    case 'รอออกใบวางบิล':
+      elementToRender = <Tag icon={<ClockCircleFilled />} color="#a569bd"> รอออกใบวางบิล </Tag>;
+      break;
+    case 'รอออกใบส่งของ':
+      elementToRender = <Tag icon={<ClockCircleFilled />} color="#d35400"> รอออกใบส่งของ </Tag>;
+      break;
+    case 'โหลดสินค้ายังไม่ครบ':
+      elementToRender = <Tag icon={<ClockCircleFilled />} color="#008080"> โหลดสินค้ายังไม่ครบ </Tag>;
+      break;
+    case 'รอโหลดสินค้า':
+      elementToRender = <Tag icon={<ClockCircleFilled />} color="#800000"> รอโหลดสินค้า </Tag>;
+      break;
+    case 'รอปริ้นใบปะหน้าถุง':
+      elementToRender = <Tag icon={<ClockCircleFilled />} color="#6495ED"> รอปริ้นใบปะหน้าถุง </Tag>;
+      break;
+    case 'ปริ้นใบปะยังไม่ครบ':
+      elementToRender = <Tag icon={<ClockCircleFilled />} color="#40E0D0"> ปริ้นใบปะยังไม่ครบ </Tag>;
+      break;
+   
     default:
       elementToRender = <Tag > Not found </Tag>;
   }
