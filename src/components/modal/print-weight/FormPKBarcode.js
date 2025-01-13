2b363d38eb25ed0734fd7a8d2847a72492250a4f
SORRANUN BUMRUNGPONG <gaiougame000000@gmail.com>
2025-01-13 17:59:51 +0700

    form

--- src/components/modal/print-weight/FormPKBarcode.js
+++ src/components/modal/print-weight/FormPKBarcode.js
@@ -2,87 +2,65 @@ import React, { forwardRef } from "react";
 import { Card, QRCode } from "antd";
 
 const keyStyled = {
-  border: "1px solid",
-  padding: "4px 8px",
-  width: "140px",
-  fontSize: "22px",
-  fontWeight: "600",
+  fontSize: "20px",
+  fontWeight: "920",
 };
 
-const valueStyled = {
-  border: "1px solid",
-  textAlign: "center",
-  fontWeight: "bold",
-  width: "250px",
-  fontSize: "28px",
-};
+
 
 const FormPKBarcode = forwardRef(({ printData }, ref) => {
-
   return (
-    <div ref={ref}>
-      {printData.map((maindata, index) => (  
-        maindata.map((data, index) => (  
-        <Card
-          title={null}
-          style={{
-            marginBottom: "8px",
-            pageBreakAfter: (1 + 1) % 3 === 0 && "always",
-          }}
-          key={1}
-        >
-          
-          <table style={{ border: "1px solid", width: "100%" }}>
-            <tr>
-              <th
-                style={{
-                  padding: "4px 8px",
-                  textAlign: "center",
-                  border: "1px solid",
-                  fontSize: "16px",
-                }}
-              >
-                {data?.county_code}
-              </th>
-            </tr>
-
-            <tr>
-              <td style={keyStyled}>
-                {data?.stname}
-                </td>
-     
-              <td style={{ ...valueStyled, width: "150px" }} rowspan="1">
-                <div
+    
+    <div ref={ref} style={{pageBreakAfter: "always"}}>
+      {printData.map((maindata, index) =>
+        maindata.map((data, index) => (
+          <Card
+            title={null}
+            style={{
+              marginBottom: "20px",
+              pageBreakAfter:  "always",
+            }}
+            key={1}
+          >
+            <table style={{  width: "100%" ,height: "100%",}}>
+              <tr>
+                <th
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    textAlign: "left",
+                    fontSize: "22px",
                   }}
+                  colSpan={2}
                 >
+                  {data?.stcode}/{data?.package_id}
+                </th>
+              </tr>
+              <tr>
+                <td style={{ ...keyStyled, fontSize: "22px",width: "60%" }} colSpan={2}>
+                  {data?.stname}
+                </td>
+                <td rowSpan={3}>
                   <QRCode
-                    size={128}
+                    size={100}
                     style={{ height: "auto" }}
                     value={data?.package_id}
-                    viewBox={`0 0 256 256`}
                   />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td style={keyStyled}>
-                {data?.weight} KG
-              </td>
-     
-            </tr>
-            <tr>
-              <td style={keyStyled}>{data?.cusname} {data?.socode}</td>
-            </tr>
-          </table>
-        </Card>
+                </td>
+              </tr>
+              <tr>
+                <td style={keyStyled}>
+                  {data?.weight} KG
+                </td>
+              </tr>
+              <tr>
+                <td style={{keyStyled, fontSize: "14px"}} colSpan={2}>
+                  {data?.cusname} {data?.socode}
+                </td>
+              </tr>
+            </table>
+          </Card>
         ))
-       ))} 
+      )}
     </div>
   );
-
 });
 export default FormPKBarcode;
